fb2ab33ff5cfbced65748b25d85148fc1d4621ca
Marcos Goudinho <marcos.goudinho@outlook.com>
2025-06-07 04:25:58 -0300

    [ADD]: Adicionar notebooks de arquitetura de medalhões

---
+++ iac/variables.tf
@@ -0,0 +1,8 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-b799124e-8756-4864-93b7-6353e0eebe53"
+}
+
+variable "location" {
+  default = "westus"
+}
